0fb02a672702b37c2de8f0faf6719df2d153bdc7
Ukarn08 <ukarn2014@outlook.fr>
2023-10-09 20:33:52 +0200

    commit

--- front/js/product.js
+++ front/js/product.js
@@ -4,33 +4,28 @@ console.log("Keys & Values:" , myKeysValues); // exemple pour un produit (Keys &
 
 //UrlSearchParams pour log les clés & valeurs dans la console
 
-const urlParams = new URLSearchParams(myKeysValues);
-
-const param1 = urlParams.get('id') 
-console.log("id" , param1);
+const params = new URL(document.location).searchParams
+const id = params.get("id")
 
 
-//UrlSearchParams pour chaque id qui est attribué a 1 produit dans la page d'acceuil 
+const url = `http://localhost:3000/api/products/${id}` //fetch l'id selectionné
 
-let url = new URL(location.href);
-let kanapPageId = url.searchParams.get("id");
+const getItems = () => {
+    fetch(url) // on recupere l'url de l'id
+    // retour json 
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+        const myPrice = (document.getElementById("price").innerHTML= data.price)
+    })
+
+}
 
 
-//
-const Kanap = document.getElementById("items"); //tout les items 'kanap' grace a cet variable
-fetch("http://localhost:3000/api/products/")
-.then((res) => res.json())
-.then((data) => {
 
-    for (let product of data) {
-        const prixKanap = product.price;
-        const descriptionKanap = product.description;
-        const titleKanap = product.title;
-        const colorsKanap = product.colors;
-        const photoKanap = product.imageUrl;
-        Kanap.innerHTML += ` <img src="${photoKanap}">`
-    }
-})
+
 
 
 
